faf9a13cea37ebec89af8bf2a8bc1403651d37c0
alielrafeiFCIH <alielrafei.fcih@gmail.com>
2019-08-04 15:30:54 +0200

    tested

--- libevolve/ga/_base.py
+++ libevolve/ga/_base.py
@@ -92,7 +92,6 @@ class GeneticAlgorithm:
 
         size = len(population)
         ind = population[0]
-        print(ind)
         for i in range(1,size,2):
             if random.random()<self.crossover_probability:
                 index_1 = random.randint(0,len(ind)-1)
@@ -175,8 +174,8 @@ class GeneticAlgorithm:
         current_generation_fitness = [[] for _ in range(nb_objectives)]
 
         for ind in current_generation:
-            dictOfWords = { chr(ord('a')+i) : ind[i] for i in range(0, len(ind) ) }
-            ind_fitness = fitness_function(**dictOfWords)
+            dict_of_params= { chr(ord('a')+i) : ind[i] for i in range(0, len(ind) ) }
+            ind_fitness = fitness_function(**dict_of_params)
             ind[0] = ind_fitness
 
         for generation_idx in range(self.nb_generations):
@@ -215,7 +214,7 @@ class NSGA_II(GeneticAlgorithm):
                  mutation_probability=0.3,
                  crossover_probability=0.5,
                  selection_size=20,
-                 seed=1234,
+                 seed=9999999999,
                  verbose=0):
         """
     Initialise a new instance of the `NSGA_II` class
@@ -254,7 +253,7 @@ class NSGA_II(GeneticAlgorithm):
                  selection_size=selection_size,
                  seed=seed,
                  verbose=verbose)
-    def evolve(self, parameters, fitness_function, objective_weights,tournament_size=10,k=5, *args, **kwargs):
+    def evolve(self, parameters, fitness_function, objective_weights,tournament_size=5,k=5, *args, **kwargs):
         """ Perform evolution on the specified parameters and objective function
 
         parameters : list
@@ -288,6 +287,8 @@ class NSGA_II(GeneticAlgorithm):
         """
         best_solution, best_score = None, 0.0
         self.parameters = parameters
+        self.fitness_function = fitness_function
+        self.objective_weights = objective_weights
         nb_params = len(parameters)
         nb_objectives = len(objective_weights)
         param_names = [p.name for p in parameters]
@@ -309,40 +310,43 @@ class NSGA_II(GeneticAlgorithm):
             select
             """
             parents = self.natural_selection(current_generation,tournament_size,k)
-            while(len(parents)!=self.population_size):
+
+            while(len(parents)<self.population_size):
                 """
                 cross over
                 """
-                self._crossover(parents)
+                parents = parents+ self._crossover(parents)
                 """
                 mutate
                 """
                 size = random.randint(0,len(parents))
 
-                parents = self._mutate(population,size)
+                parents = parents + self._mutate(population,size)
 
-            current_generation = current_generation+parents
+            current_generation = current_generation + parents
+
             """
             Fast Non Dominated Sort
             """
+
             offsprings = self._fast_non_dominated_sort(current_generation)
             """
             Crowding Distance 
             """
+
             offsprings = self._crowding_distance(offsprings)
 
-            offsprings = offsprings.sort()
+            offsprings.sort()
 
-            offsprings = offsprings[0:self.population_size]
+            current_generation = offsprings[0:self.population_size]
 
-            current_generation = []
-            for i in range(0,self.population_size):
-                current_generation.append(offsprings[i])
             best_solution = current_generation
+
         return best_solution, best_score, self.history
 
     def _crowding_distance(self,front):
         distance = [0 for i in range(0, len(front))]
+
         for obj_fun in range(2,len(self.fitness_function)+2):
             front = np.array(front)
             front_1 = np.sort(front[:,obj_fun])
@@ -352,39 +356,44 @@ class NSGA_II(GeneticAlgorithm):
             distance[0]=4444444444444444
             distance[len(front)-1]=4444444444444444
             for pos in range(1,len(front_1)-1):
-                distance[pos] = distance[pos] + (front_1[pos + 1] - front_1[pos - 1]) / (
-                            mx_value-mn_value)
+                if((front_1[pos + 1] - front_1[pos - 1])==0 and (mx_value - mn_value)==0):
+                    distance[pos]+=0
+                else:
+                    distance[pos] = distance[pos] + (front_1[pos + 1] - front_1[pos - 1]) / (mx_value-mn_value)
+
         re_sort_order = np.argsort(front_1_index)
         for index in re_sort_order:
-            front[index,1] = distance[index]
+            if(index is not np.NaN):
+                front[index,1] = distance[index]
 
         front = [list(ind) for ind in front]
         return front
 
     def _dominat(self,ind1,ind2):
-        for obj_score in range(0,len(ind1)-len(self.fitness_function)):
-            if(ind1[obj_score]>ind2[obj_score]or ind1[obj_score]>=ind2[obj_score]):
-                print("")
+        for obj_score in range(2,2+len(self.fitness_function)):
+            if(ind1[obj_score]>=ind2[obj_score]):
+                continue
             else: return False
         return True
 
     def _fast_non_dominated_sort(self, population):
-        Dominent = [[[]] for i in range(0, len(population))]
-        front = [[[]]]
-        Dominent_count = {ind:0 for ind in population}
-        rank = {ind: -1 for ind in population}
+        population = self.Evaulate(population,self.fitness_function)
+        Dominent = {tuple(ind):[] for ind in population}
+        front = [[]]
+        Dominent_count = {tuple(ind):0 for ind in population}
+        rank = {tuple(ind): -1 for ind in population}
 
         for ind_1 in range(0, len(population)):
-            Dominent[ind_1] = []
-            Dominent_count[ind_1] = 0
-            for ind_2 in range(ind_1+1, len(population)):
+            Dominent[tuple(population[ind_1])] = []
+            Dominent_count[tuple(population[ind_1])] = 0
+            for ind_2 in range(0, len(population)):
                 if (self._dominat(population[ind_1],population[ind_2])):
-                    if population[ind_2] not in Dominent[population[ind_1]]:
-                        Dominent[population[ind_1]].append(population[ind_2])
+                    if population[ind_2] not in Dominent[tuple(population[ind_1])]:
+                        Dominent[tuple(population[ind_1])].append(population[ind_2])
                 else:
-                    Dominent_count[population[ind_1]] = Dominent_count[population[ind_1]] + 1
-            if Dominent_count[population[ind_1]] == 0:
-                rank[population[ind_1]] = 0
+                    Dominent_count[tuple(population[ind_1])] = Dominent_count[tuple(population[ind_1])] + 1
+            if Dominent_count[tuple(population[ind_1])] == 0:
+                rank[tuple(population[ind_1])] = 0
                 if population[ind_1] not in front[0]:
                     front[0].append(population[ind_1])
 
@@ -392,29 +401,31 @@ class NSGA_II(GeneticAlgorithm):
         while (front[itre] != []):
             Q = []
             for p in front[itre]:
-                for q in Dominent[p]:
-                    Dominent_count[q] = Dominent_count[q] - 1
-                    if (Dominent_count[q] == 0):
-                        rank[q] = itre + 1
+                for q in Dominent[tuple(p)]:
+                    Dominent_count[tuple(q)] = Dominent_count[tuple(q)] - 1
+                    if (Dominent_count[tuple(q)] == 0):
+                        rank[tuple(q)] = itre + 1
                         if q not in Q:
                             Q.append(q)
+
             itre = itre + 1
             front.append(Q)
-
         del front[len(front) - 1]
         population = []
         for front_i in front:
             for ind in front_i:
-                ind[0] = rank[ind]
+                ind[0] = rank[tuple(ind)]
                 population.append(ind)
         return population
 
     def Evaulate(self, population,fitness_function):
         for ind in population:
-            iter = 1
+            iter = 2
             for obj_fn in fitness_function:
-                ind[iter]=(obj_fn(**ind.key_params))
+                dict_of_params = {chr(ord('a') + i): ind[i]*self.objective_weights[i] for i in range(3, len(ind))}
+                ind[iter]=obj_fn(**dict_of_params)
                 iter +=1
+        return population
     def natural_selection(self,individuals,tournsize,k):
         """Select the best individual among *tournsize* randomly chosen
             individuals, *k* times. The list returned contains
@@ -441,12 +452,12 @@ class NSGA_II(GeneticAlgorithm):
         """
         offsprings = population
         for ind in offsprings:
-            for i in range(1, len(ind)-len(self.parameters)):
-                if random.random() < self.mutation_probability:
-                    ind[i] = self.parameters[i].get_rand_value()
+            if random.random() < self.mutation_probability:
+                index = random.randint(3,len(ind)-1)
+                ind[index] = self.parameters[index].get_rand_value()
+        return offsprings
 
-
-def _crossover(self, population):
+    def _crossover(self, population):
         """ The Crossover operator is analogous to reproduction and biological crossover.
             In this more than one parent is selected and one or more off-springs are produced using the genetic material of the parents.
             Crossover is usually applied in a GA with a high probability – pc
@@ -459,9 +470,10 @@ def _crossover(self, population):
         the offsprings of the current generation
         """
         size = len(population)
-        ind = population[0][0]
+        ind = population[0]
         for i in range(1, size, 2):
             if random.random() < self.crossover_probability:
-                index_1 = random.randint(len(self.fitness_function), len(ind))
-                index_2 = random.randint(len(self.fitness_function), len(ind))
+                index_1 = random.randint(3, len(ind)-1)
+                index_2 = random.randint(3, len(ind)-1)
                 population[i - 1][index_1], population[i][index_2] = population[i][index_2], population[i - 1][index_1]
+        return population
